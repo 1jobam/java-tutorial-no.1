fdb55d05d1dab251bc37c960a2f35a928dfce0d8
1jobam <limm2770@naver.com>
2019-12-03 20:09:34 +0900

    2019.12.03

--- src/d_array/Score.java
+++ src/d_array/Score.java
@@ -19,34 +19,109 @@ public class Score {
 		 * 
 		 */
 	
-		String[] student = new String[21];
-		String[] ranks = new String[4];
+//		String[] student = new String[21];
+//		String[] ranks = new String[4];
+//		
+//		int[][] subject = new int[21][6];
+//		int[] su = new int[21];
+//		int[] rank = new int[21];
+//		
+//		double[] avg = new double[21];
+//		
+//		String[] name = {"김지선", "이누리", "박종민", "정대석", "정재영", "강현철", "주향한", "윤창훈", "연은주", "김령환", "구한나", "백종빈", "이병훈", "강현지", "김경운", "이지형", "이예림", "이승재", "민태홍", "정보람", "박진영"};
+//		String[] sc = {"Java", "Oracle", "HTML", "CSS", "JQuery", "JSP"};
+//		
+//		System.out.println("순위" + "\t이름" + "\t" + sc[0] + sc[1] + sc[2] + sc[3] + sc[4] + sc[5] + "\t총점" + "\t평균" + "\t등급");		
+//		for(int i = 0; i < subject.length; i++){
+//			for(int j = 0; j < subject[i].length; j++){
+//				subject[i][j] = (int)(Math.random() * 51) + 50;
+//				su[i] += subject[i][j];
+//				avg[i] = (double)su[i] / subject[i].length;
+//				
+//			}
+//			System.out.print(rank[i] + "등" + "\t" + name[i] + "\t" + Arrays.toString(subject[i]) + "\t" + su[i] + "\t" + Math.round(avg[i]) + "\t");
+//			System.out.println();
+//		}
+//		
+//		for(int i = 0; i < subject.length; i++){
+//			for(int j = 0; j < subject[i].length; j++){
+//				subject[i][j] = (int)(Math.random() * 51) + 50;
+//			}
+//		}
+//		System.out.print("과목 점수 총 평점    :  " );
+//		System.out.println();
 		
-		int[][] subject = new int[21][6];
-		int[] su = new int[21];
-		int[] rank = new int[21];
+		String[] students = {"김지선", "이누리", "박종민", "정대석", "정재영", "강현철", "주향한", "윤창훈", "연은주", "김령환", "구한나", "백종빈", "이병훈", "강현지", "김경운", "이지형", "이예림", "이승재", "민태홍", "정보람", "박진영"};
+		String[] subjects = {"Java", "Oracle", "HTML", "CSS", "JQuery", "JSP"};
+		double[][] scores = new double[students.length][subjects.length + 3];
 		
-		double[] avg = new double[21];
-		
-		String[] name = {"김지선", "이누리", "박종민", "정대석", "정재영", "강현철", "주향한", "윤창훈", "연은주", "김령환", "구한나", "백종빈", "이병훈", "강현지", "김경운", "이지형", "이예림", "이승재", "민태홍", "정보람", "박진영"};
-		String[] sc = {"Java", "Oracle", "HTML", "CSS", "JQuery", "JSP"};
-		
-		System.out.println("순위" + "\t이름" + "\t" + sc[0] + sc[1] + sc[2] + sc[3] + sc[4] + sc[5] + "\t총점" + "\t평균" + "\t등급");		
-		for(int i = 0; i < name.length; i++){
-			for(int j = 0; j < subject[i].length; j++){
-				subject[i][j] = (int)(Math.random() * 51) + 50;
-				su[i] += subject[i][j];
-				avg[i] = (double)su[i] / subject[i].length;
+		//점수를 입력한다.
+		for(int i = 0; i < scores.length; i++){
+			scores[i][0] = 1; // 석차
+			for(int j = 0; j < subjects.length; j++){
+				scores[i][j + 1] = (int)(Math.random() * 51) + 50;
 			}
-			System.out.print(rank[i] + "등" + "\t" + name[i] + "\t" + Arrays.toString(subject[i]) + "\t" + su[i] + "\t" + Math.round(avg[i]) + "\t");
+		}
+		
+		//총점과 평균을 구한다.
+		for(int i = 0; i < scores.length; i++){
+			for(int j = 0; j < subjects.length; j++){
+				scores[i][scores[i].length - 2]
+								+= scores[i][j + 1]; //총점
+			}
+			scores[i][scores[i].length - 1]
+					= (int)(scores[i][scores[i].length - 2]
+							/ subjects.length * 100 + 0.5)
+							/ 100.0; //평균
+		}
+		
+		//석차를 구한다.
+		for(int i = 0; i < scores.length; i++){
+			for(int j = 0; j < scores.length; j++){
+				if(scores[i][scores[i].length - 1] < scores[j][scores[j].length - 1]){
+					scores[i][0]++;
+				}
+			}
+		}
+		
+		//정렬한다.
+		for(int i = 0; i < scores.length - 1; i++){
+			for(int j = i + 1; j < scores.length; j++){
+				if(scores[i][0] > scores[j][0]){
+					double[] temp = scores[i];
+					scores[i] = scores[j];
+					scores[j] = temp;
+					
+					String tempName = students[i];
+					students[i] = students[j];
+					students[j] = tempName;
+				}
+			}
+		}
+		
+		//출력한다.
+		System.out.print("석차\t이름");
+		for(int i = 0; i < subjects.length; i++){
+			System.out.print("\t" + subjects[i]);
+		}
+		System.out.println("\t총점\t평균");
+		
+		for(int i = 0; i < scores.length; i++){
+			System.out.print((int)scores[i][0] + "\t" + students[i]);
+			for(int j = 1; j < scores[i].length; j++){
+				System.out.print("\t" + scores[i][j]);
+			}
 			System.out.println();
-		}		
-		
-		
-		
-		
-		
-		
+		}
+		System.out.print("과목별 평균 : ");
+		for(int i = 1; i <= subjects.length; i++){
+			double sum = 0;
+			for(int j = 0; j < scores.length; j++){
+				sum += scores[j][i];
+			}
+			double avg = (int)(sum / scores.length * 100 + 0.5) / 100.0;
+			System.out.print("\t" + avg);
+		}
 	}
 
 }
--- src/z_exam/Exam05.java
+++ src/z_exam/Exam05.java
@@ -6,11 +6,35 @@ public class Exam05 {
 
 	public static void main(String[] args) {
 		
-		
+//		char[][] star = {
+//		{'*','*',' ',' ',' '},
+//		{'*','*',' ',' ',' '},
+//		{'*','*','*','*','*'},
+//		{'*','*','*','*','*'}
+//		};
+//		char[][] result = new char[star[0].length][star.length];
+//		for(int i=0; i < star.length;i++) {
+//		for(int j=0; j < star[i].length;j++) {
+//		System.out.print(star[i][j]);
+//		}
+//		System.out.println();
+//		}
+//		System.out.println();
+//		for(int i=0; i < star.length;i++) {
+//		for(int j=0; j < star[i].length;j++) {
+//			result[i][star.length-i-1] = star[i][j];
+//		}
+//		}
+//		for(int i=0; i < result.length;i++) {
+//		for(int j=0; j < result[i].length;j++) {
+//		System.out.print(result[i][j]);
+//		}
+//		System.out.println();
+//		}
 		
 		/*[5-1] 다음은 배열을 선언하거나 초기화한 것이다. 잘못된 것을 고르고 그 이유를 설명
 		하시오.
-		(a, d, e)
+		(d, e)
 		a. int[] arr[];
 		b. int[] arr = {1,2,3,};
 		c. int[] arr = new int[5];
@@ -18,9 +42,14 @@ public class Exam05 {
 		e. int arr[5];
 		f. int[] arr[] = new int[3][];*/
 		
+//		int[] arr[];
+//		int[] arr = {1, 2, 3,};
+//		int[] arr = new int[5];
+//		int[] arr = new int[5];
+//		arr[0] = 1;
+//		arr[1] = 2;
+//		int arr[5];
 //		int[] arr[] = new int[3][];
-//		System.out.println(arr);
-	
 		
 		/*[5-2] 다음과 같은 배열이 있을 때, arr[3].length의 값은 얼마인가?
 		int[][] arr = { //d int[] -- 크기  int [][] -- 크기안의 리터럴 값
@@ -37,40 +66,39 @@ public class Exam05 {
 //				{20, 20, 20, 20},
 //				{30, 30}
 //		};
-//			System.out.println(arr.length);
-//		for (int i = 0; i < arr.length; i++) {
+//		System.out.println(Arrays.toString(arr[3]));
+		
+//		int[][] arr = {
+//						{5, 5, 5, 5, 5},
+//						{10, 10, 10},
+//						{20, 20, 20, 20},
+//						{30, 30}	
+//						};
+//		for(int i = 0; i < arr.length; i++){
 //			System.out.println("arr[" + i + "].length : " + arr[i].length);
-//			for (int j = 0; j < arr[i].length; j++) {
+//			for(int j = 0; j < arr[i].length; j++){
 //				System.out.println("arr[" + i + "][" + j + "].length : " + arr[i][j]);
 //			}
 //		}
 		
+
+		
 		
 	/*	[5-3] 배열 arr에 담긴 모든 값을 더하는 프로그램을 완성하시오.
-		[연습문제]/ch5/Exercise5_3.java
-		class Exercise5_3
-		{
-		 public static void main(String[] args)
-		 {
+		[연습문제]/ch5/Exercise5_3.java 
 		int[] arr = {10, 20, 30, 40, 50};
 		int sum = 0;
 		
 		 (1) 알맞은 코드를 넣어 완성하시오. 
 		System.out.println("sum="+sum);
-		}
-		}
-		 
-		24 Java의 정석定石 2판 - 연습문제 풀이
-		[실행결과]
-		totat=325
-		average=16.25*/
-		
+	*/
 //		int[] arr = {10, 20, 30, 40, 50};
 //		int sum = 0;
-//		for(int i = 0; i < arr.length; i++) {
+//		for(int i = 0; i < arr.length; i++){
 //			sum += arr[i];
-//			System.out.println("sum="+sum);
 //		}
+//		System.out.println("sum = " + sum + " 이다");
+
 		
 		
 		
@@ -80,10 +108,6 @@ public class Exam05 {
 		
 		/*[5-4] 2차원 배열 arr2에 담긴 모든 값의 총합과 평균을 구하는 프로그램을 완성하시오.
 		[연습문제]/ch5/Exercise5_4.java
-		class Exercise5_4
-		{
-		 public static void main(String[] args)
-		 {
 		int[][] arr = {
 		{ 5, 5, 5, 5, 5},
 		{10,10,10,10,10},
@@ -95,44 +119,37 @@ public class Exam05 {
 		
 		(1) 알맞은 코드를 넣어 완성하시오. 
 		System.out.println("totat="+total);
-		System.out.println("average="+average);
-		} // end of main
-		} // end of class 
-		Java의 정석定石 2판 - 연습문제 풀이 25
-		[실행결과]
-		486*/
+		System.out.println("average="+average);  */
+
+		
 		
 //		int[][] arr = {
-//		{ 5, 5, 5, 5, 5},
-//		{10,10,10,10,10},
-//		{20,20,20,20,20},
-//		{30,30,30,30,30}
-//		};
+//						{ 5, 5, 5, 5, 5},
+//						{10,10,10,10,10},
+//						{20,20,20,20,20},
+//						{30,30,30,30,30}
+//						};
 //		int total = 0;
 //		float average = 0;
-//		System.out.println("arr.length : " + arr.length);
+//		
 //		int temp = 0;
-//		for (int i = 0; i < arr.length; i++) {
-//			System.out.println("arr[" + i + "].length : " + arr[i].length);
-//			for (int j = 0; j < arr[i].length; j++) {
+//		for(int i = 0; i < arr.length; i++){
+//			for(int j = 0; j < arr[i].length; j++){
 //				total += arr[i][j];
 //			}
 //			temp += arr[i].length;
 //		}
-//		average = (total / (float) temp);
+//		average = total / (float)temp;
 //		
-//		System.out.println("totat=" + total);
-//		System.out.println("average=" + average);
+//		System.out.println("totat="+total);
+//		System.out.println("average="+average);
 		
-		
-		
+
+
 		
 		/*[5-5] 다음은 1과 9사이의 중복되지 않은 숫자로 이루어진 3자리 숫자를 만들어내는 프
 		로그램이다. (1)~(2)에 알맞은 코드를 넣어서 프로그램을 완성하시오.
 		[참고] Math.random()을 사용했기 때문에 실행결과와 다를 수 있다.
-		[연습문제]/ch5/Exercise5_5.java
-		class Exercise5_5 {
-		public static void main(String[] args) {
 		int[] ballArr = {1,2,3,4,5,6,7,8,9};
 		int[] ball3 = new int[3];
 		// 배열 ballArr의 임의의 요소 2개를 골라서 위치를 바꾼다. 20번 반복
@@ -147,41 +164,28 @@ public class Exam05 {
 		// 배열 ballArr의 앞에서 3개의 수를 배열 ball3로 복사한다.
 		 (2) 
 		for(int i=0;i<ball3.length;i++) {
-		System.out.print(ball3[i]);
-		}
-		} // end of main
-		} // end of class 
-		26 Java의 정석定石 2판 - 연습문제 풀이
-		[실행결과]
-		money=2680
-		500원: 5
-		100원: 1
-		50원: 1
-		10원: 3*/
+		System.out.print(ball3[i]); */
 		
+
 //		int[] ballArr = {1,2,3,4,5,6,7,8,9};
 //		int[] ball3 = new int[3];
 //		// 배열 ballArr의 임의의 요소 2개를 골라서 위치를 바꾼다. 20번 반복
-//		for(int x=0;x < 20;x++) {
-//		int i = (int)(Math.random() * ballArr.length);
-//		int j = (int)(Math.random() * ballArr.length);
+//		for(int x = 0; x < 20; x++) {
+//			int i = (int)(Math.random() * ballArr.length);
+//			int j = (int)(Math.random() * ballArr.length);
 //		int tmp = 0;
 //		tmp = ballArr[i];
 //		ballArr[i] = ballArr[j];
 //		ballArr[j] = tmp;
-//
+//		}
+//		for(int i = 0; i < ballArr.length; i++){
+//		System.out.println(ballArr[i]);
 //		}
 //		// 배열 ballArr의 앞에서 3개의 수를 배열 ball3로 복사한다.
-//		for(int i = 0; i < ballArr.length; i++) {
-//			System.out.println(ballArr[i]);
-//		}
-//		System.out.println();
-//		System.arraycopy(ballArr, 0, ball3, 0, 3);
-//		for(int i=0;i<ball3.length;i++) {
+//		for(int i = 0; i < ball3.length; i++) {
+//			ball3[i] = ballArr[i];
 //		System.out.print(ball3[i]);
 //		}
-		
-		
 		
 		
 		
@@ -192,33 +196,22 @@ public class Exam05 {
 		라. 단, 가능한 한 적은 수의 동전으로 거슬러 주어야한다. (1)에 알맞은 코드를 넣어서
 		프로그램을 완성하시오.
 		[Hint] 나눗셈 연산자와 나머지 연산자를 사용해야 한다.
-		[연습문제]/ch5/Exercise5_6.java
-		class Exercise5_6 {
-		public static void main(String args[]) {
 		 // 큰 금액의 동전을 우선적으로 거슬러 줘야한다.
 		int[] coinUnit = {500, 100, 50, 10};
 		int money = 2680;
 		System.out.println("money="+money);
 		for(int i=0;i<coinUnit.length;i++) {
-		
 		 (1) 알맞은 코드를 넣어 완성하시오. 
-		}
-		} // main
-		}
-		Java의 정석定石 2판 - 연습문제 풀이 27*/
+		} */
 		
-		 // 큰 금액의 동전을 우선적으로 거슬러 줘야한다.
+		// 큰 금액의 동전을 우선적으로 거슬러 줘야한다.
 //		int[] coinUnit = {500, 100, 50, 10};
 //		int money = 2680;
 //		System.out.println("money="+money);
-//		for(int i=0;i<coinUnit.length;i++) {
+//		for(int i = 0; i < coinUnit.length; i++) {
 //			System.out.println(coinUnit[i] + "원: " + money / coinUnit[i]);
 //			money = money % coinUnit[i];
-//		}
-		
-		
-		
-		
+//			}
 		
 		
 		
@@ -290,18 +283,50 @@ public class Exam05 {
 		2**
 		4*****/
 		
+		/* (1) 아래의 로직에 맞게 코드를 작성하시오.
+		1. 금액(money)을 동전단위로 나눠서 필요한 동전의 개수(coinNum)를 구한다.
+		2. 배열 coin에서 coinNum만큼의 동전을 뺀다.
+		(만일 충분한 동전이 없다면 배열 coin에 있는 만큼만 뺀다.)
+		3. 금액에서 동전의 개수(coinNum)와 동전단위를 곱한 값을 뺀다.
+		*/
+		
+//		if(args.length!=1) {
+//			System.out.println("USAGE: java Exercise5_7 3120");
+//			System.exit(0);
+//		}
+//			
+//		// 문자열을 숫자로 변환한다. 입력한 값이 숫자가 아닐 경우 예외가 발생한다.
+//		int money = Integer.parseInt(args[0]);
+//		
+//		System.out.println("money="+money);
+//		
+//		int[] coinUnit = {500, 100, 50, 10 }; // 동전의 단위
+//		int[] coin = {5, 5, 5, 5}; // 단위별 동전의 개수
+//			
+//			for(int i = 0; i < coinUnit.length; i++) {
+//				int coinNum = 0;
+//				coinNum = coin[i];
+//				System.out.println(coinNum);
+//				System.out.println(coinUnit[i]+"원: "+coinNum);
+//			}
+//			
+//			if(money > 0) {
+//				System.out.println("거스름돈이 부족합니다.");
+//				System.exit(0); // 프로그램을 종료한다.
+//			}
+//			
+//				System.out.println("=남은 동전의 개수 =");
+//			
+//			for(int i = 0; i < coinUnit.length; i++) {
+//				System.out.println(coinUnit[i]+"원:"+coin[i]);
+//			}
 		
 		
 		
 		
 		
-		
-		
-		
-		/*[5-8] 다음은 배열 answer에 담긴 데이터를 읽고 각 숫자의 개수를 세어서 개수만큼 ‘*’ 을 찍어서 그래프를 그리는 프로그램이다. (1)~(2)에 알맞은 코드를 넣어서 완성하시오.
+/*		[5-8] 다음은 배열 answer에 담긴 데이터를 읽고 각 숫자의 개수를 세어서 개수만큼 ‘*’ 을 찍어서 그래프를 그리는 프로그램이다. (1)~(2)에 알맞은 코드를 넣어서 완성하시오.
 		[연습문제]/ch5/Exercise5_8.java
-		class Exercise5_8 {
-		 public static void main(String[] args) {
 		int[] answer = { 1,4,4,3,1,4,4,2,1,3,2 };
 		int[] counter = new int[4];
 		for(int i=0; i < answer.length;i++) {
@@ -312,41 +337,31 @@ public class Exam05 {
 		 (2) 알맞은 코드를 넣어 완성하시오. 
 		System.out.println();
 		}
-		 } // end of main
-		} // end of class 
-		Java의 정석定石 2판 - 연습문제 풀이 29
-		[실행결과] **
-		**
-		*****
-		*****
-		****
-		****
-		**
-		**
-		***/
-		
+
+		[실행결과] 
+		3***
+		2**
+	intelligence	2**
+		4****
+*/
+			
 //		int[] answer = { 1,4,4,3,1,4,4,2,1,3,2 };
 //		int[] counter = new int[4];
 //		for(int i=0; i < answer.length;i++) {
-//			counter[answer[i]-1]++;
+//			counter[answer[i] - 1]++;
 //		}
 //		for(int i=0; i < counter.length;i++) {
-//		System.out.println(counter[i]);
-//			for(int j = 0; j < counter[i]; j++) {
-//				System.out.println("*");
+//				System.out.print(counter[i]);
+//			for(int j = 0; j < counter[i]; j++){
+//				System.out.print("*");
 //			}
-//		System.out.println();
+//			System.out.println();
 //		}
-		
-		
-		
 		
 		
 		
 		/*[5-9] 주어진 배열을 시계방향으로 90도 회전시켜서 출력하는 프로그램을 완성하시오.
 		[연습문제]/ch5/Exercise5_9.java
-		class Exercise5_9 {
-		 public static void main(String[] args) {
 		char[][] star = {
 		{'*','*',' ',' ',' '},
 		{'*','*',' ',' ',' '},
@@ -372,45 +387,35 @@ public class Exam05 {
 		System.out.print(result[i][j]);
 		}
 		System.out.println();
-		}
-		 } // end of main
-		} // end of class 
-		30 Java의 정석定石 2판 - 연습문제 풀이
-		0 1 2 3 4 5 6 7 8 9
-		q w e r t y u i o p
-		u v w x y z
-		} ; : , . /
-		a b c d e f g h i j k l m n o p q r s t ` ~ ! @ # $ % ^ & * ( ) - _ + = | [ ] {
-		[실행결과]
-		src:abc123
-		result:`~!wer*/
-		
+		} */
 		
 //		char[][] star = {
-//		{'*','*',' ',' ',' '},
-//		{'*','*',' ',' ',' '},
-//		{'*','*','*','*','*'},
-//		{'*','*','*','*','*'}
-//		};
+//				{'*','*',' ',' ',' '},
+//				{'*','*',' ',' ',' '},
+//				{'*','*','*','*','*'},
+//				{'*','*','*','*','*'}
+//				};
 //		char[][] result = new char[star[0].length][star.length];
-//		for(int i=0; i < star.length;i++) {
-//		for(int j=0; j < star[i].length;j++) {
-//		System.out.print(star[i][j]);
+//		for (int i = 0; i < star.length; i++) {
+//			for (int j = 0; j < star[i].length; j++) {
+//				System.out.print(star[i][j]);
+//			}
+//			System.out.println();
 //		}
 //		System.out.println();
+//		for (int i = 0; i < star.length; i++) {
+//			for (int j = 0; j < star[i].length; j++) {
+//				result[j][i] = star[3 - i][j];
+//			}
 //		}
-//		System.out.println();
-//		for(int i=0; i < star.length;i++) {
-//		for(int j=0; j < star[i].length;j++) {
-//			result[i][star.length-i-1] = star[i][j];
+//		for (int i = 0; i < result.length; i++) {
+//			for (int j = 0; j < result[i].length; j++) {
+//				System.out.print(result[i][j]);
+//			}
+//			System.out.println();
 //		}
-//		}
-//		for(int i=0; i < result.length;i++) {
-//		for(int j=0; j < result[i].length;j++) {
-//		System.out.print(result[i][j]);
-//		}
-//		System.out.println();
-//		}
+		
+
 		
 		
 		
@@ -420,8 +425,6 @@ public class Exam05 {
 		/*[5-10] 다음은 알파벳과 숫자를 아래에 주어진 암호표로 암호화하는 프로그램이다.
 		(1)에 알맞은 코드를 넣어서 완성하시오.
 		[연습문제]/ch5/Exercise5_10.java
-		class Exercise5_10 {
-		public static void main(String[] args) {
 		char[] abcCode =
 		 { '`','~','!','@','#','$','%','^','&','*',
 		 '(',')','-','_','+','=','|','[',']','{',
@@ -438,39 +441,36 @@ public class Exam05 {
 		 
 		}
 		System.out.println("src:"+src);
-		System.out.println("result:"+result);
-		} // end of main
-		} // end of class 
-		Java의 정석定石 2판 - 연습문제 풀이 31
-		[실행결과]
-		100 100 100 300
-		 20 20 20 60
-		 30 30 30 90
-		 40 40 40 120
-		 50 50 50 150
-		240 240 240 720*/
+		System.out.println("result:"+result); */
 		
+//		int b = '1';
+//		System.out.println(b);
 		
-		
-//				char[] abcCode =
-//		 { '`','~','!','@','#','$','%','^','&','*',
-//		 '(',')','-','_','+','=','|','[',']','{',
-//		 '}',';',':',',','.','/'};
-//		 // 0 1 2 3 4 5 6 7 8 9
-//		char[] numCode = {'q','w','e','r','t','y','u','i','o','p'};
-//		String src = "abc123";
-//		String result = "";
-//		 // 문자열 src의 문자를 charAt()으로 하나씩 읽어서 변환 후 result에 저장
-//		for(int i=0; i < src.length();i++) {
-//		char ch = src.charAt(i);
-//		 if('a' <= ch && ch <= 'z') {
-//			 result+=abcCode[ch-'a'];
-//		 }else {
-//			 result+=numCode[ch-'0'];
-//		 }
-//		}
-//		System.out.println("src:"+src);
-//		System.out.println("result:"+result);
+
+//			char[] abcCode =
+//			 { '`','~','!','@','#','$','%','^','&','*',
+//			 '(',')','-','_','+','=','|','[',']','{',
+//			 '}',';',':',',','.','/'};
+//			
+//				// 0 1 2 3 4 5 6 7 8 9
+//			
+//			char[] numCode = {'q','w','e','r','t','y','u','i','o','p'};
+//			
+//			String src = "abc123";
+//			String result = "";
+//			
+//			 // 문자열 src의 문자를 charAt()으로 하나씩 읽어서 변환 후 result에 저장
+//			
+//			for(int i=0; i < src.length();i++) {
+//			char ch = src.charAt(i);
+//				if('a' <= ch && ch <= 'z'){
+//					result += abcCode[ch - 'a'];
+//				}else{
+//					result += numCode[ch - '0'];
+//				}
+//			}
+//			System.out.println("src:"+src);
+//			System.out.println("result:"+result);
 		
 		
 		
@@ -515,6 +515,7 @@ public class Exam05 {
 				, {40, 40, 40}
 				, {50, 50, 50}
 				};
+		
 				int[][] result = new int[score.length+1][score[0].length+1];
 				for(int i=0; i < score.length;i++) {
 				for(int j=0; j < score[i].length;j++) {
